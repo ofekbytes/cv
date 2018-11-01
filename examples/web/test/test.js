a7f8a8f50434d2d2cb909111e5a9ad15b9d35625
Yaron Kessler <Yaron.Kessler@brinks.com>
2018-11-01 16:59:55 +0200

    test

--- examples/web/test/test.js
+++ examples/web/test/test.js
@@ -5,12 +5,14 @@ var globalNumber = 0;
 function initialize()
 {
     globalNumber = 22;
+    console.log(globalNumber);
 }
 
 
 function update()
 {
     globalNumber = 33;
+    console.log(globalNumber);
 }
 
 initialize();
