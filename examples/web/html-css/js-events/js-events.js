21c7b35987b3c60407736d352ccb43d85404d2b9
Yaron Kessler <Yaron.Kessler@brinks.com>
2018-11-27 18:17:51 +0200

    test

--- examples/web/html-css/js-events/js-events.js
+++ examples/web/html-css/js-events/js-events.js
@@ -26,11 +26,13 @@ function  fnCheckboxEvent()
 {
     if (document.getElementById('barcode').checked == true)
     {            
+        document.getElementById("textFirstName").value = "on";
         document.getElementById("output").innerText = "barcode checked ... id == " + document.getElementById("barcode").id;
-        console.log("barcode checked ... id == " + document.getElementById("barcode").id);                
+        console.log("barcode checked ... id == " + document.getElementById("barcode").id);           
     }
     else
     {
+        document.getElementById("textFirstName").value = "off";
         document.getElementById("output").innerText = "barcode ... id == " + document.getElementById("barcode").id;
         console.log("barcode ... id == " + document.getElementById("barcode").id);    
     }
